aa20bfd24e17aab537ad38e814dc5b64cc7139ce
Abdulaziz6665 <mr.yunusobod@gmail.com>
2022-05-23 09:15:24 +0500

    sa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('working ?'));
+app.get('/', (req, res) => res.send('test ?'));
 
 app.listen(4500, () => console.log('Example app listening on port 4500!'));
